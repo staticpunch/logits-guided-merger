6b7874ae41e36280d5bfaa5618b6b704216d8248
staticpunch <nguyenthanhdo.contact@gmail.com>
2024-12-20 08:39:23 +0000

    add statistic calculation

--- compare_models.py
+++ compare_models.py
@@ -16,11 +16,14 @@ from IO import (
     get_layer_signatures, 
     save_checkpoint
 )
+import argparse
+from datetime import datetime
 
 def compute_theta(
     v0: Union[np.ndarray, torch.Tensor],
     v1: Union[np.ndarray, torch.Tensor],
     eps: float = 1e-8,
+    device: str = "cpu"
 ):
     """
     Spherical linear interpolation
@@ -32,62 +35,57 @@ def compute_theta(
         v1 (np.ndarray): Final vector
         DOT_THRESHOLD (float): Threshold for considering the two vectors as
                                colinear. Not recommended to alter this.
+        device (str): Device to load tensors, "cpu" or "cuda".
+
     Returns:
         v2 (np.ndarray): Interpolation vector between v0 and v1
     """
-    is_torch = False
-    if not isinstance(v0, np.ndarray):
-        is_torch = True
-        v0 = v0.detach().cpu().float().numpy()
-    if not isinstance(v1, np.ndarray):
-        is_torch = True
-        v1 = v1.detach().cpu().float().numpy()
 
-    # Copy the vectors to reuse them later
-    v0_copy = np.copy(v0)
-    v1_copy = np.copy(v1)
+    # Convert inputs to PyTorch tensors and move to the specified device
+    if not isinstance(v0, torch.Tensor):
+        v0 = torch.from_numpy(v0)
+    if not isinstance(v1, torch.Tensor):
+        v1 = torch.from_numpy(v1)
+
+    v0 = v0.float().to(device)
+    v1 = v1.float().to(device)
+
+    # Copy the vectors to reuse them later   
+    v0_copy = v0.clone()
+    v1_copy = v1.clone()
+
+    lengths = [
+        torch.norm(v0_copy).cpu().item(),
+        torch.norm(v1_copy).cpu().item()
+    ]
+    
+    euclide = torch.norm(v0_copy - v1_copy)
+    dot = torch.sum(v0_copy * v1_copy)
 
     # Normalize the vectors to get the directions and angles
     v0 = normalize(v0, eps)
     v1 = normalize(v1, eps)
-    # import ipdb; ipdb.set_trace()
 
     # Dot product with the normalized vectors (can't use np.dot in W)
-    dot = np.sum(v0 * v1)
+    cosine = torch.sum(v0 * v1)
 
     # Calculate initial angle between v0 and v1
-    theta_0 = np.arccos(dot)
-    degree = np.degrees(theta_0)
-    
-    # return float(dot)
-    # return float(theta_0)
-    return float(degree)
+    theta_0 = torch.arccos(torch.clamp(cosine, -1.0, 1.0))
+    # degree = np.degrees(theta_0)
 
-def compute_l2(v0, v1):
-    is_torch = False
-    if not isinstance(v0, np.ndarray):
-        is_torch = True
-        v0 = v0.detach().cpu().float().numpy()
-    if not isinstance(v1, np.ndarray):
-        is_torch = True
-        v1 = v1.detach().cpu().float().numpy()
+    return {
+        "lengths": lengths,
+        "dot": dot.cpu().item(),
+        "L2": euclide.cpu().item(),
+        "cosine": cosine.cpu().item(),
+        "theta": theta_0.cpu().item()
+    }
 
-    dist = np.linalg.norm(v0 - v1)
-    return float(dist)
-
-
-def maybe_torch(v: np.ndarray, is_torch: bool):
-    if is_torch:
-        return torch.from_numpy(v)
+def normalize(v: torch.Tensor, eps: float):
+    norm_v = torch.norm(v)
+    if norm_v > eps:
+        v = v / norm_v
     return v
-
-
-def normalize(v: np.ndarray, eps: float):
-    return v / np.linalg.norm(v)
-    # norm_v = np.linalg.norm(v)
-    # if norm_v > eps:
-    #     v = v / norm_v
-    # return v
 
 def compute_layers_thetas(tensors, merge_config):
     """
@@ -101,11 +99,6 @@ def compute_layers_thetas(tensors, merge_config):
         Dict[str, torch.Tensor]: Merged model weights.
     """
     assert len(tensors) == 2
-    conditions = merge_config["parameters"]["t"]
-    conditions = {c['filter']: c['value'] for c in conditions}
-
-    layer_begin, layer_end = merge_config["layer_range"]
-    num_layers = layer_end - layer_begin
     
     weight_names = [key for key in tensors[0].keys()]
     thetas = {}
@@ -114,10 +107,10 @@ def compute_layers_thetas(tensors, merge_config):
         tensor_b = tensors[1][weight_name]
         theta = compute_theta(
             v0=tensor_a,
-            v1=tensor_b
+            v1=tensor_b,
+            device="cuda:7"
         )
         thetas.update({weight_name: theta})
-        # torch.testing.assert_close(tensor_merged, tensor_computed)
     return thetas
 
 def run_compute(
@@ -127,32 +120,47 @@ def run_compute(
     model_paths = [x['model'] for x in merge_config['sources']]
     layer_signatures = get_layer_signatures(model_paths[0])
     output_dir = merge_config["output_dir"]
-    tmp_dir = os.path.join(output_dir, "tmp_dir")
     model_thetas = {}
-        
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
 
     ## Merge models
     for signature in (
         pbar := tqdm(
             layer_signatures,
-            desc="Merging ...",
+            desc="Computing distance ...",
             ncols=150
         )
     ):
-        pbar.set_description(f"Merging {signature}")
+        pbar.set_description(f"Computing distance {signature}")
         models_tensors = [load_tensors(path, signature) for path in model_paths]
         thetas = compute_layers_thetas(models_tensors, merge_config)
         model_thetas.update({signature: thetas})
         
-    with open("thetas.json", "w") as f:
+    statistics_path = os.path.join(output_dir, "statistics.json")
+    with open(statistics_path, "w") as f:
         json.dump(model_thetas, f, indent=4)
 
 if __name__ == "__main__":
-    # CONFIG_FILE = "slerp-config-custom.yaml"
-    CONFIG_FILE = "config-03.yaml"
-    with open(CONFIG_FILE, "r", encoding="utf-8") as f:
+    parser = argparse.ArgumentParser(description="Compute thetas between two models.")
+    parser.add_argument(
+        "--config",
+        type=str,
+        required=True,
+        help="Path to the configuration file (YAML).",
+    )
+    args = parser.parse_args()
+
+    with open(args.config, "r", encoding="utf-8") as f:
         merge_config = yaml.safe_load(f)
-        
+
+    output_dir = merge_config["output_dir"]
+    
+    # Create a subdirectory with the current date and time
+    current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+    output_dir = os.path.join(output_dir, current_time)
+    merge_config["output_dir"] = output_dir
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    shutil.copy(args.config, output_dir)
+
     run_compute(merge_config)
